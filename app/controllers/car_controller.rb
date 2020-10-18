f8b513e49b0250064baa49fb22d032eef969dc73
goce-arsoski <goce_ars@yahoo.com>
2020-10-18 23:10:53 +0200

    Create controller Car

---
+++ app/controllers/car_controller.rb
@@ -0,0 +1,44 @@
+class CarController < ApplicationController
+  before_action :find_car, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @car = Car.all.order('created_at DESC')
+  end
+
+  def show
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+  end
+
+  def edit
+  end
+
+  def update
+    if @car.update(car_params)
+      redirect_to @car
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @car.destroy
+    redirect_to cars_path
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :year, :vin, :mileage, :transmission, :drive_type, :color)
+  end
+
+  def find_car
+    @car = Car.find_by(params[:id])
+  end
+end
---
+++ spec/views/car/index.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "car/index.html.erb", type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
